6c8f11853d77963fa19c33cc9978b649ba7f5f52
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-09 16:32:15 -0400

    added api

--- UI.js
+++ UI.js
@@ -1,5 +1,6 @@
-const url = 'https://developers.zomato.com/documentation#!/common/collections'
-let headers = { headers: { 'x-api-key': 'e1d1a4db60a524fc8bf6a94f67d7c8b0' } }
+const urlDC = 'https://developers.zomato.com/api/v2.1/cities?q=Washington%2C%20DC&count=1'
+let topRest = 'https://developers.zomato.com/api/v2.1/collections?city_id=283'
+let headers = { headers: { 'user-key': 'e1d1a4db60a524fc8bf6a94f67d7c8b0' } }
 let menuBar = document.querySelector('.threelines')
 let parent = document.querySelector('.container')
 menuBar.addEventListener('click', function () {
@@ -7,11 +8,10 @@ menuBar.addEventListener('click', function () {
 })
 
 function getRestaurants() {
-    fetch(url, headers)
-        .then(res => {
-            console.log('hello')
-            console.log(res)
-            return res.json()
-        })
+    fetch(topRest, headers)
+        .then(res => res.json())
+        .then(res => console.log(res))
 }
+
+getRestaurants()
 
