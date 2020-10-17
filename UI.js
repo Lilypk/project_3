e78390513887224fcc51ef379ecfa0037c442947
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-17 17:19:03 -0400

    finished

--- UI.js
+++ UI.js
@@ -1,61 +1,49 @@
-const breedURL = 'https://danieledminster.com/dogbreedlist.php'
-const affenURL = 'https://dog.ceo/api/breed/affenpinscher/images'
-// const afrURL = 'https://dog.ceo/api/breed/African/images'
-// const airURL = 'https://dog.ceo/api/breed/Airedale/images'
-// const akiURL = 'https://dog.ceo/api/breed/Akita/images'
-// const appURL = 'https://dog.ceo/api/breed/Appenzeller/images'
-let menuBar = document.querySelector('.threelines')
-let parent = document.querySelector('.container')
-let list = document.querySelector('.unorderedlist')
-let imageplacement = document.querySelector('.imgcontainer')
-let imageone = ''
-let listItem = []
+const breedURL = "https://danieledminster.com/dogbreedlist.php";
+const affenURL = "https://dog.ceo/api/breed/affenpinscher/images";
+let menuBar = document.querySelector(".threelines");
+let parent = document.querySelector(".container");
+let list = document.querySelector(".unorderedlist");
+let imageplacement = document.querySelector(".imgcontainer");
+let imageone = "";
+let listItem = [];
 
-menuBar.addEventListener('click', function () {
-    parent.classList.toggle('menudrops')
-})
+menuBar.addEventListener("click", function () {
+  parent.classList.toggle("menudrops");
+});
 
 function getDogList(breedURL) {
-    fetch(breedURL)
-        .then(res => res.json())
-        .then(res => {
-            for (let i = 0; i < 5; i++) {
-                let listItem = document.createElement('li')
+  fetch(breedURL)
+    .then((res) => res.json())
+    .then((res) => {
+      for (let i = 0; i < 5; i++) {
+        let listItem = document.createElement("li");
 
-                listItem.className = 'breed'
-                listItem.innerText = res[i]
-                listItem.addEventListener('click', function() {
-                    let baseURL = `https://dog.ceo/api/breed/${res[i]}/images`
-                    getDogImage(baseURL)
-                })
-                list.appendChild(listItem)
-            }
-        })
+        listItem.className = "breed";
+        listItem.innerText = res[i];
+        listItem.addEventListener("click", function () {
+          let baseURL = `https://dog.ceo/api/breed/${res[i]}/images`;
+          getDogImage(baseURL);
+        });
+        list.appendChild(listItem);
+      }
+    });
 }
-getDogList(breedURL)
+getDogList(breedURL);
 
 function getDogImage(affenURL) {
-    fetch(affenURL) 
-    .then(res => res.json())
-    .then(res => {
-        console.log(res)
-            //grab the first three images
-            //create event listener on first list 
-            //add image to the text
-            //place the image in the center
-            imageplacement.innerHTML = ''
-            for (let i = 0; i < 3; i++) {
-                
-                let imageOne = document.createElement('img')
-               
-                imageOne.setAttribute('src', res.message[i])
-                
-                imageplacement.appendChild(imageOne)
-            }
-            
-            })
-        
+  fetch(affenURL)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      //grab the first three images
+      //create event listener on first list
+      //add image to the text
+      //place the image in the center
+      imageplacement.innerHTML = "";
+      for (let i = 0; i < 6; i++) {
+        let imageOne = document.createElement("img");
+        imageOne.setAttribute("src", res.message[i]);
+        imageplacement.appendChild(imageOne);
+      }
+    });
 }
-
-// getDogImage(affenURL)
-
